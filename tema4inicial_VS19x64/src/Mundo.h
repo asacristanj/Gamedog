012862e174adf1ca7b9f8a1409097103173f5ddf
abelbague <a.bague@alumnos.upm.es>
2021-05-03 15:44:21 +0100

    prueba_pang

---
+++ tema4inicial_VS19x64/src/Mundo.h
@@ -0,0 +1,14 @@
+
+class Mundo
+{
+public: 
+	void tecla(unsigned char key);
+	void inicializa();
+	void rotarOjo();
+	void mueve();
+	void dibuja();
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+};
