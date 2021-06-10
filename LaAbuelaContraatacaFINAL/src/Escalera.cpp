c75e15d69232a7c7b4d99d44229f2b925e1437c2
raulherranzrodriguez <raul.herranz.rodriguez@alumnos.upm.es>
2021-06-10 21:22:26 +0200

    intento arreglo vista de escalera

--- LaAbuelaContraatacaFINAL/src/Escalera.cpp
+++ LaAbuelaContraatacaFINAL/src/Escalera.cpp
@@ -27,19 +27,25 @@ void Escalera::dibujar() {
 	glEnd();
 	glEnable(GL_LIGHTING);*/
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/escaleras.png").id);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 	glColor3f(1, 1, 1);
+	/*
 	glTexCoord2d(0, 1); glVertex2f(limder1.x, limder1.y);
 	glTexCoord2d(1, 1); glVertex2f(limizq1.x, limizq1.y);
 	glTexCoord2d(1, 0); glVertex2f(limizq2.x, limizq2.y);
 	glTexCoord2d(0, 0); glVertex2f(limder2.x, limder2.y);
-	/*glVertex3d(limder1.x, limder1.y, -5);
-	glVertex3d(limizq1.x, limizq1.y, -5);
-	glVertex3d(limizq2.x, limizq2.y, -5);
-	glVertex3d(limder2.x, limder2.y, -5);*/
+	*/
+	
+	glTexCoord2d(0, 1);glVertex3d(limizq1.x, limizq1.y, 0);
+	glTexCoord2d(1, 1);glVertex3d(limder2.x, limizq1.y,0);
+	glTexCoord2d(1, 0);glVertex3d(limder2.x, limizq2.y, 0);
+	glTexCoord2d(0, 0);glVertex3d(limizq1.x, limizq2.y, 0);
+
 	glEnd();
 	glEnable(GL_LIGHTING);
 	glDisable(GL_TEXTURE_2D);
